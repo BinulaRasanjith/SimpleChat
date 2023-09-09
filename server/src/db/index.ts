1f3e723f5cebc9cfaac9db7bc8bb6dde21871122
Binula Rasanjith <binula28@gmail.com>
2023-09-09 12:06:37 +0530

    db configure file created

---
+++ server/src/db/index.ts
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize";
+import { Dialect } from "sequelize/lib/sequelize";
+
+const dbname: string = process.env.DB_NAME || "simple_chat";
+const dbUsername: string = process.env.DB_USERNAME || "postgres";
+const dbPassword: string = process.env.DB_PASSWORD || "1234";
+const dbHost: string = process.env.DB_HOST || "localhost";
+const dbDialect: Dialect = process.env.DB_DIALECT as Dialect || "postgres"; 
+
+// console.log(dbname, dbUsername, dbPassword, dbHost, dbDialect);
+ 
+const sequelize: Sequelize = new Sequelize(
+    dbname,
+    dbUsername,
+    dbPassword,
+    {
+        host: dbHost,
+        dialect: dbDialect,
+        logging: false
+    } 
+);
+
+export default sequelize;
