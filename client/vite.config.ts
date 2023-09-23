99ad989806272f7304cb87dcf5b56d6fa0ce7491
Binula Rasanjith <binula28@gmail.com>
2023-09-24 05:13:09 +0530

    vite port configuration added and mui icons installed

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// Set the port
+const port = 5000;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port,
+  },
 })
