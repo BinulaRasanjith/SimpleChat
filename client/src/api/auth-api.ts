8a722a470c399ef6b7814bff200cdc34ddc5a7f5
Binula Rasanjith <binula28@gmail.com>
2023-10-02 16:37:41 +0530

    username availability checking added for signup

--- client/src/api/auth-api.ts
+++ client/src/api/auth-api.ts
@@ -12,10 +12,15 @@ const signup = async (username: string, password: string) => {
     return await axios.post("/auth/signup", { username, password });
 };
 
+const checkUsername = async (username: string) => {
+    return await axios.get(`/auth/check-username/${username}`);
+};
+
 const auth = {
     login,
     logout,
     signup,
+    checkUsername,
 };
 
 export default auth;
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -13,9 +13,43 @@ import {
 import styleProps from "./styleProps";
 import { useState } from "react";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
+import api from "../api";
 
 const Signup = () => {
 	const [showPassword, setShowPassword] = useState(false);
+	const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+
+	const handelUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		// Set username trimming whitespace
+		setUsername(e.target.value.trim());
+
+		// Check if username is available
+		if (e.target.value.trim() === "") return;
+		api.auth
+			.checkUsername(e.target.value.trim())
+			.then((res) => {
+				if (res.data.ok) {
+					console.log("Username is available");
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	const handelPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setPassword(e.target.value);
+	};
+
+	const handelConfirmPasswordChange = (
+		e: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setConfirmPassword(e.target.value);
+	};
 
 	const handleClickShowPassword = () => setShowPassword(!showPassword);
 
@@ -29,23 +63,26 @@ const Signup = () => {
 				</Typography>
 				<TextField
 					id="username"
+					name="username"
 					label="Username"
+					value={username}
+					onChange={handelUsernameChange}
 					variant="outlined"
 					sx={styleProps.formTextfield}
 				/>
 				<FormControl sx={styleProps.formTextfield} variant="outlined">
-					<InputLabel htmlFor="outlined-adornment-password">
-						Password
-					</InputLabel>
+					<InputLabel htmlFor="password">Password</InputLabel>
 					<OutlinedInput
-						id="outlined-adornment-password"
+						id="password"
+						name="password"
+						value={password}
+						onChange={handelPasswordChange}
 						type={showPassword ? "text" : "password"}
 						endAdornment={
 							<InputAdornment position="end">
 								<IconButton
 									aria-label="toggle password visibility"
 									onClick={handleClickShowPassword}
-									// onMouseDown={handleMouseDownPassword}
 									edge="end"
 								>
 									{showPassword ? <VisibilityOff /> : <Visibility />}
@@ -55,13 +92,28 @@ const Signup = () => {
 						label="Password"
 					/>
 				</FormControl>
-				<TextField
-					id="confirm-password"
-					label="Confirm Password"
-					type="password"
-					variant="outlined"
-					sx={styleProps.formTextfield}
-				/>
+				<FormControl sx={styleProps.formTextfield} variant="outlined">
+					<InputLabel htmlFor="confirmPassword">Confirm Password</InputLabel>
+					<OutlinedInput
+						id="confirmPassword"
+						name="confirmPassword"
+						value={confirmPassword}
+						onChange={handelConfirmPasswordChange}
+						type={showConfirmPassword ? "text" : "password"}
+						endAdornment={
+							<InputAdornment position="end">
+								<IconButton
+									aria-label="toggle password visibility"
+									onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+									edge="end"
+								>
+									{showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+								</IconButton>
+							</InputAdornment>
+						}
+						label="Confirm Password"
+					/>
+				</FormControl>
 				<Button variant="contained">Signup</Button>
 			</Paper>
 		</Box>
